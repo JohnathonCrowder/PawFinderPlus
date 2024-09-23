d89dddd43c78a2ec8e5a24b6b36e09245f3ef38d
Johnathon Crowder <139363360+JohnathonCrowder@users.noreply.github.com>
2024-09-23 13:30:29 -0500

    Updated Home page

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
+from app.models import Litter, BlogPost, DogStatus
+
 
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def home():
-    return render_template('index.html')
+    featured_litters = Litter.query.filter_by(is_public=True).order_by(Litter.date_of_birth.desc()).limit(6).all()
+    recent_blog_posts = BlogPost.query.filter_by(is_published=True).order_by(BlogPost.created_at.desc()).limit(3).all()
+    return render_template('index.html', featured_litters=featured_litters, recent_blog_posts=recent_blog_posts)
 
 @bp.route('/about')
 def about():
